1f74f3e4195b5786c9366367004ea946cea53575
thanhkhon03 <2121050666@student.humg.edu.vn>
2025-01-13 11:24:07 +0700

    up-to-date commit

--- web-app-basic/main.go
+++ web-app-basic/main.go
@@ -13,7 +13,7 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
-	log.Println("Starting server on :8080")
+	log.Println("Starting server on :8080") // Chay tai host:8080
 	err := http.ListenAndServe(":8080", mux)
 	log.Fatal(err)
 }
