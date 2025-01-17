1a1601e60f859bccfac1da675a8c882c5e0a673f
thanhkhon03 <2121050666@student.humg.edu.vn>
2025-01-17 11:53:29 +0700

    loading

--- go-jwt/handler/handler_user.go
+++ go-jwt/handler/handler_user.go
@@ -1,74 +1,74 @@
 package handler
 
-import(
+import (
 	"encoding/json"
 	"fmt"
 	config "go-jwt/config"
 	driver "go-jwt/driver"
-	models "go-jwt/models"
+	models "go-jwt/model"
 	repoImpl "go-jwt/repository/repoimpl"
 	"net/http"
 	"strings"
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
-
 )
 
-var jwtKey := []byte("abcdefghijklmnoq") // "day la secret key"
+var jwtKey = []byte("abcdefghijklmnopq") // "day la secret key"
 
-//Playload
-type Claims struct{
-	Email string `json:"email"`
+// Playload
+type Claims struct {
+	Email       string `json:"email"`
 	DisplayName string `json:"display_Name"`
 	jwt.StandardClaims
 }
 
-func Register(w http.ResponseWriter, r *http.Request){
+func Register(w http.ResponseWriter, r *http.Request) {
 	var regData models.RegistrationData
 	err := json.NewDecoder(r.Body).Decode(&regData)
-	if err != nil{
-		ResponseErr(w , http.StatusBadRequest)
+	if err != nil {
+		ResponseErr(w, http.StatusBadRequest)
 		return
 	}
 
-	//Kiem tra thong tin user da ton tai trong he thong 
+	//Kiem tra thong tin user da ton tai trong he thong
 	_, err = repoImpl.NewUserRepo(driver.Mongo.Client.
-		              Database(config.DB_NAME)).
-					  FindUserByEmail(regData.Email)
-	
-	//Neu co tra ve StatusConfilict				  
-	if err != nil models.ERR_USER_NOT_FOUND {
+		Database(config.DB_NAME)).
+		FindUserByEmail(regData.Email)
+
+	//Neu co tra ve StatusConfilict
+	if err != models.ERR_USER_NOT_FOUND {
 		ResponseErr(w, http.StatusConflict)
 		return
 	}
 
 	user := models.User{
-		Email: regData.Email,
-		Password: regData.Password,
-		DisplayName: reData.DisplayName,
-	} 
+		Email:       regData.Email,
+		Password:    regData.Password,
+		DisplayName: regData.DisplayName,
+	}
 	err = repoImpl.NewUserRepo(driver.Mongo.Client.
-		            Database(config.DB_NAME)).Insert(user)
+		Database(config.DB_NAME)).Insert(user)
 	if err != nil {
-        ResponseErr(w, http.StatusInternalServerError)
-		return 
-	}	
-    //Neu ham tren chay thanh cong se chay den ham nay
+		ResponseErr(w, http.StatusInternalServerError)
+		return
+	}
+	//Neu ham tren chay thanh cong se chay den ham nay
 	var tokenString string
 	tokenString, err = GenToken(user)
 	if err != nil {
 		ResponseErr(w, http.StatusInternalServerError)
 		return
 	}
-    // Ham tra ve Token cho nguoi dung
+	// Ham tra ve Token cho nguoi dung
 	ResponseOk(w, models.RegisterResponse{
-		Token: tokenString,
+		Token:  tokenString,
 		Status: http.StatusOK,
 	})
+
 }
 
-func Login(w http.ResponseWriter, r *http.Request){
+func Login(w http.ResponseWriter, r *http.Request) {
 	var loginData models.LoginData
 	err := json.NewDecoder(r.Body).Decode(&loginData)
 	if err != nil {
@@ -78,24 +78,94 @@ func Login(w http.ResponseWriter, r *http.Request){
 
 	var user models.User
 	user, err = repoImpl.NewUserRepo(driver.Mongo.Client.
-		                 Database(config.DB_NAME)).
-						 CheckLoginInfo(loginData.Email, loginData.Password)
-	if err != nil{
+		Database(config.DB_NAME)).
+		CheckLoginInfo(loginData.Email, loginData.Password)
+	if err != nil {
 		ResponseErr(w, http.StatusUnauthorized)
 		return
 	}
 
 	var tokenString string
 	tokenString, err = GenToken(user)
-	if err != nil{
+	if err != nil {
 		ResponseErr(w, http.StatusInternalServerError)
 		return
 	}
 	ResponseOk(w, models.RegisterResponse{
-		Token: tokenString,
-		Status:http.StatusOK,
+		Token:  tokenString,
+		Status: http.StatusOK,
 	})
 
 }
 
-func GetUser
+func GetUser(w http.ResponseWriter, r *http.Request) {
+	tokenHeader := r.Header.Get("Authorization")
+
+	if tokenHeader == "" {
+		ResponseErr(w, http.StatusForbidden)
+		return
+	}
+
+	splinted := strings.Split(tokenHeader, " ") // Bearer jwt_token
+	if len(splinted) != 2 {
+		ResponseErr(w, http.StatusForbidden)
+		return
+	}
+
+	tokenPart := splinted[1]
+	tk := &Claims{}
+
+	token, err := jwt.ParseWithClaims(tokenPart, tk, func(token *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+
+	if err != nil {
+		fmt.Println(err)
+		ResponseErr(w, http.StatusInternalServerError)
+		return
+	}
+
+	if token.Valid {
+		ResponseOk(w, token.Claims)
+	}
+}
+
+func GenToken(user models.User) (string, error) { //Ham tao ra JWT
+	expirationTime := time.Now().Add(120 * time.Second)
+	claims := &Claims{
+		Email:       user.Email,
+		DisplayName: user.DisplayName,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims) // <=> Signature = Header + Playload
+	return token.SignedString(jwtKey)
+}
+
+func ResponseErr(w http.ResponseWriter, statusCode int) {
+	jData, err := json.Marshal(models.Error{
+		Status:  statusCode,
+		Message: http.StatusText(statusCode),
+	})
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jData)
+}
+
+func ResponseOk(w http.ResponseWriter, data interface{}) {
+	if data == nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	jData, err := json.Marshal(data)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jData)
+}
--- go-jwt/main.go
+++ go-jwt/main.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"fmt"
-	//"net/http"
-	//"go-jwt/handler"
+	config "go-jwt/config"
 	"go-jwt/driver"
-	repoImpl "go-jwt/repository/repoimpl"
+	"go-jwt/handler"
+	"net/http"
 )
 
 func main() {
-	mongo := driver.ConnectMongoDB(DB_USER, DB_PASS)
+	driver.ConnectMongoDB(config.DB_USER, config.DB_PASS)
 
-	userRepo := repoImpl.NewUserRepo(mongo.Client.Database(DB_NAME))
+	//userRepo := repoImpl.NewUserRepo(mongo.Client.Database(DB_NAME)) Unlock this to run check login
 
 	/*user := models.User{
 		Email:       "admin@example.com",
@@ -22,6 +22,12 @@ func main() {
 	if err == nil { //Kiem tra neu khong loi se in ra chuoi String
 		fmt.Println("Insert ok")
 	}*/
-	user, _ := userRepo.CheckLoginInfo("admin@example.com", "1234567890")
-	fmt.Println(user)
+	/*user, _ := userRepo.CheckLoginInfo("admin@example.com", "1234567890")  Cau lenh de check login info
+	fmt.Println(user)*/
+	http.HandleFunc("/login", handler.Login)       //Log in vao server bang du lieu vua dang ki
+	http.HandleFunc("/register", handler.Register) // Dang ki tai khoan vao server
+	http.HandleFunc("/user", handler.GetUser)      // Hien thi len cho nguoi dung
+
+	fmt.Println("Server running [:8000]")
+	http.ListenAndServe(":8000", nil)
 }
